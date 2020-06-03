6d696d6519c5ed6d4913116f080357fa23250104
topalek <topalek@mail.ru>
2020-06-03 16:58:31 +0300

    validator

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import dateFilter from '@/filters/date.filter';
-import "./registerServiceWorker";
+import Vue from "vue"
+import Vuelidate from "vuelidate"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import dateFilter from '@/filters/date.filter'
+import "./registerServiceWorker"
 import "materialize-css/dist/js/materialize.min"
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuelidate)
 
 Vue.filter('date', dateFilter)
 
