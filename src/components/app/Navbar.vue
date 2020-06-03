d0c5e0149291e31fb580da87d3c1f9f477053987
topalek <topalek@mail.ru>
2020-06-03 12:04:05 +0300

    navbar improvement + date filter

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -1,35 +1,63 @@
 <template>
-  <nav class="navbar orange lighten-1">
+<nav class="navbar orange lighten-1">
     <div class="nav-wrapper">
-      <div class="navbar-left">
-        <a href="#" @click.prevent="$emit('click')">
-          <i class="material-icons black-text">dehaze</i>
-        </a>
-        <span class="black-text">12.12.12</span>
-      </div>
+        <div class="navbar-left">
+            <a href="#" @click.prevent="$emit('click')">
+                <i class="material-icons black-text">dehaze</i>
+            </a>
+            <span class="black-text">{{date | date('datetime')}}</span>
+        </div>
 
-      <ul class="right hide-on-small-and-down">
-        <li>
-          <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
-            USER NAME
-            <i class="material-icons right">arrow_drop_down</i>
-          </a>
+        <ul class="right hide-on-small-and-down">
+            <li>
+                <a class="dropdown-trigger black-text" href="#" data-target="dropdown" ref="dropdown">
+                    USER NAME
+                    <i class="material-icons right">arrow_drop_down</i>
+                </a>
 
-          <ul id="dropdown" class="dropdown-content">
-            <li>
-              <a href="#" class="black-text">
-                <i class="material-icons">account_circle</i>Профиль
-              </a>
+                <ul id="dropdown" class="dropdown-content">
+                    <li>
+                        <router-link to="/profile" class="black-text">
+                            <i class="material-icons">account_circle</i>Профиль
+                        </router-link>
+                    </li>
+                    <li class="divider" tabindex="-1"></li>
+                    <li>
+                        <a href="#" class="black-text" @click.prevent="logout">
+                            <i class="material-icons">assignment_return</i>Выйти
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="divider" tabindex="-1"></li>
-            <li>
-              <a href="#" class="black-text">
-                <i class="material-icons">assignment_return</i>Выйти
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+        </ul>
     </div>
-  </nav>
+</nav>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        date: new Date(),
+        interval: null,
+        dropdown: null,
+    }),
+    methods: {
+        logout() {
+            console.log("Logout");
+            this.$router.push("/login?message=logout");
+        }
+    },
+    mounted() {
+        this.interval = setInterval(() => {
+            this.date = new Date()
+        }, 1000)
+        this.dropdown = window.M.Dropdown.init(this.$refs.dropdown, {});
+    },
+    beforeDestroy() {
+        clearInterval(this.interval)
+        if(this.dropdown && this.dropdown.destroy){
+          this.dropdown.destroy()
+        }
+    }
+};
+</script>
